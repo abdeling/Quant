066bc31b90f480a3aaec6afce76138a520badf1e
abdeling <69136039+abdeling@users.noreply.github.com>
2024-12-26 20:00:56 -0500

    Update American-options.py

--- American-options.py
+++ American-options.py
@@ -59,7 +59,7 @@ def option_price(S0, K, T, r, sigma, steps,option_style ="european" ,option_type
 
 
 # Streamlit App
-st.subheader("Option Pricing Dashboard (Binomial Model)")
+st.subheader("Option Pricing Tool (Binomial Model)")
 
 # User Inputs
 st.sidebar.header("Input Parameters")
